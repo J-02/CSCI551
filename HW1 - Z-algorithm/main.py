cf58a4ccb3c94cd30a38d1b45bc7a9948276a751
jonhenk <Jon.henk3@gmail.com>
2023-09-06 17:15:26 -0600

    HW1 close to finish

--- HW1 - Z-algorithm/main.py
+++ HW1 - Z-algorithm/main.py
@@ -1,17 +1,21 @@
 import sys
 
+# runs in terminal with "python main.py test.fasta search"
+# can be run on any .fasta file in directory
+# This code reads the fasta file to dictionary
+
 def main(file, search):
     # Search each sequence in file for string and return z array and exact pattern matching
     motifs = readFasta(file)
 
     for key, seq in motifs.items():
-        s = search+"#"+seq
-        zarray = zalg(seq)
-        exact = zalg(s)[len(search)+1:]
-        indices = [i for i, v in enumerate(exact) if v == len(search)]
+        s = search+"#"+seq  # creates concatenated string with search term
+        zarray = zalg(seq)  # zarray without search term
+        exact = zalg(s)  # zarray with search term
+        indices = [i for i, v in enumerate(exact) if v == len(search)]  # indices where exact pattern starts
         print(f"Searching {key}:{seq} for {search}")
         print(f"Z-array: {zarray}")
-        print(f"Exact pattern matching at indicies: {indices}")
+        print(f"Exact pattern matching starting at indicies: {indices}\n\n")
 
 def readFasta(file):
     motifs = dict()
@@ -33,24 +37,26 @@ def readFasta(file):
 
 
 def zalg(s):
-    ## returns Z array length of input sequence
+    # returns Z array length of input sequence
     n = len(s)
-    Z = [0] * n
+    Z = [0] * n  # empty z-array the length of sequence
     Z[0] = n
-
     l = r = 0
-    for i in range(1, n):
-        if i <= r:
-            Z[i] = min(r - i + 1, Z[i - l])
 
-        while i + Z[i] < n and s[Z[i]] == s[i + Z[i]]:
-            Z[i] += 1
+    for k in range(1, n):
 
-        if i + Z[i] - 1 > r:
-            l, r = i, i + Z[i] - 1
+        if k <= r:
+            Z[k] = min(r - k + 1, Z[k - l])
+
+        while k + Z[k] < n and s[Z[k]] == s[k + Z[k]]:
+            Z[k] += 1
+
+        if k + Z[k] - 1 > r:
+            l, r = k, k + Z[k] - 1
 
     return Z
 
+# to run in terminal
 if __name__ == "__main__":
     file = (sys.argv[1])
     search = (sys.argv[2])
