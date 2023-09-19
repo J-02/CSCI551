84d306ee278671a3137c85f3ef5c24ea56b69811
jonhenk <Jon.henk3@gmail.com>
2023-09-19 17:19:41 -0600

    ready for tadeo to code

--- HW2/myAlign.py
+++ HW2/myAlign.py
@@ -8,13 +8,42 @@ import numpy as np
 def main(scores, file):
     # Search each sequence in file for string and return z array and exact pattern matching
     motifs = readFasta(file)
+    # get S and T
+    match = scores[0]
+    # score for match
+    mismatch = scores[1]
+    # score for mismatch
+    insdel = scores[2]
+    # score for insertion/deletion
 
+    # adding gaps to the beginning of each sequence
     T = "-" + motifs[1]
     S = "-" + motifs[0]
+
+    # debug print
     print(T)
     print(S)
+
+    # initialize V
     V = np.zeros(shape = (len(T), len(S)))
+
+    # debug print
     print(V)
+
+    # iterating through each letter in T
+    for i, Ti in enumerate(T):
+        # i is index of Ti in T
+        # iterating through each letter in S
+        for j, Sj in enumerate(S):
+            # j is index of Sj in S
+            if Ti == Sj:
+                # if equal add match score
+                V[i,j] += match
+            elif i != j:
+                # TODO: add rest of algorithm here
+                pass
+
+
 
 
 
@@ -32,6 +61,8 @@ def readFasta(file):
     print(motifs)
     return motifs
 
+# test run function
+# scores in tuple (a, b, c)
 main((2,-1,-1), "test.fa")
 
 # to run in terminal
